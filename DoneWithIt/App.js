b50482dc1d2051556dd6a5fc68d4561aa5007dbb
miaplesyrup <99581223+miaplesyrup@users.noreply.github.com>
2023-11-07 17:05:24 +0800

    styling! :)

--- DoneWithIt/App.js
+++ DoneWithIt/App.js
@@ -1,20 +1,64 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView} from 'react-native';
-
+import { 
+  StyleSheet, 
+  Text, 
+  TouchableHighlight, 
+  Image,
+  SafeAreaView,
+  Alert,
+  Button,
+  Platform,
+  View,
+  Dimensions
+} from 'react-native';
 export default function App() { 
+  const handlePress = () => console.log("Text pressed")
+  console.log(Dimensions.get("screen"));
   return (
     <SafeAreaView style={styles.container}>
-      <Text numberOfLines={1}>Hello to my first react native app:) yuhooo! no matter what happens that's it. bahala na</Text>
-      
+      <View style={{
+        backgroundColor: 'dodgerblue',
+        width: '100%',
+        height: '30%'
+      }}>
+        <Text numberOfLines={1} onPress={handlePress}> Hello to my first react native app:) yuhooo! no matter what happens that's it. bahala na
+        </Text>
+        <TouchableHighlight onPress={()=> console.log("Image tapped")}>
+          <Image 
+            blurRadius={2}
+            fadeDuration={3000}
+            borderRadius={30}
+            source={{ 
+              width: 200,
+              height: 300,
+              uri: "https://picsum.photos/id/1/200/300"
+              }} 
+            /> 
+        </TouchableHighlight>
+        <Button 
+        color="orange"
+          title="Click Me" 
+          onPress={()=> 
+            Alert.alert("Hallo!", "Geht's dir gut?", [
+              {text:"Ja", onPress: () => console.log("Ja")},
+              {text: "Nein", onPress: () => console.log("Nein")}
+            ])
+            // Alert.prompt("Hallo", "wie geht's'?", text => console.log(text))
+          }
+        />
+      </View>
     </SafeAreaView>
   );
 }
+
+const containerStyle= { backgroundColor: "dodgerblue"};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // alignItems: 'center',
+    // justifyContent: 'center',
+    paddingTop: Platform.OS === 'android' ? 30 : 0,
   },
 });
